f42754425c8b11881b3c5ec39645c123ce5616b4
Shivansh Sahu <sahuishivansh447@gmail.com>
2026-09-12 17:29:30 +0530

    messages

---
+++ demo.js
@@ -0,0 +1,29 @@
+// Basic JavaScript Program
+
+let name = "Shivansh";
+let age = 25;
+
+console.log("Hello, World!");
+console.log("My name is " + name);
+console.log("My age is " + age);
+
+// Function
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(10, 20);
+
+console.log("Sum =", result);
+
+// Condition
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+// Loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Number:", i);
+}
